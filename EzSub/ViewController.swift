79af6843b55afe538d486f7800bb44a1ea3e3fcd
Mingyong Cai <Mingyong Cai>
2020-03-30 18:53:52 -0400

    Initial Commit

---
+++ EzSub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EzSub
+//
+//  Created by Wei Nie on 3/30/20.
+//  Copyright © 2020 Terrycai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EzSubTests/EzSubTests.swift
@@ -0,0 +1,34 @@
+//
+//  EzSubTests.swift
+//  EzSubTests
+//
+//  Created by Wei Nie on 3/30/20.
+//  Copyright © 2020 Terrycai. All rights reserved.
+//
+
+import XCTest
+@testable import EzSub
+
+class EzSubTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
