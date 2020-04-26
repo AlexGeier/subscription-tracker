c0ef666034eca518ed1940b0a7bc11179d51105f
josephwon0310 <josephwon0310@gmail.com>
2020-04-26 17:40:27 -0400

    smol fix. will branch out after this push

--- EzSub/DynamicSubscriptionDetailViewController.swift
+++ EzSub/DynamicSubscriptionDetailViewController.swift
@@ -23,20 +23,6 @@ class DynamicSubscriptionDetailViewController: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
-    @IBAction func onDeleteSubscription(_ sender: Any) {
-        var flag = 0// yes or no flag
-        let alert = UIAlertController(title: "Alert", message: "Are you sure?", preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {
-            (action: UIAlertAction!) in
-            flag = 1
-            
-        }))
-        
-        alert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
-        
-        self.present(alert, animated: true)
-    }
     
 }
 
